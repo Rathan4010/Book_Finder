bd2639be62897f2ef380d1e028c6a88d280c7fc1
Rathan4010 <rathanmaraa1@gmail.com>
2025-08-28 13:38:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [books, setBooks] = useState([]);
+
+  const handleSearch = () => {
+    console.log("Searching for:", searchTerm);
+
+    fetch(`https://openlibrary.org/search.json?title=${searchTerm}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Books:", data.docs);
+        setBooks(data.docs)
+      })
+      .catch((err) => console.error("Error:", err));
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="top-section">
+        <div className="heading"><i>book finder</i></div>
+        <div className="searchbar">
+          <label htmlFor="book-name">Book Name </label>
+          <input
+            id="book-name"
+            type="text"
+            placeholder="enter the book name"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyDown={(e) => e.key === "Enter" && handleSearch()} // Enter key support
+          />
+        </div>
+      </div>
+      {/* results*/}
+      <div className="results">
+        {books.map((book, index) => (
+          <div key={index} className="book">
+            {book.cover_i ? (
+        <img
+          src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
+          alt={book.title}
+          className="book-cover"
+        />
+      ) : (
+        <div className="no-cover">No Image</div>
+      )}
+            <h3>{book.title}</h3>
+            <p>
+              {book.author_name ? book.author_name.join(", ") : "Unknown Author"}
+            </p>
+          </div>
+        ))}
+      </div>
+
+      {/* Background image section */}
+      <div className="background-section"></div>
     </div>
   );
 }
